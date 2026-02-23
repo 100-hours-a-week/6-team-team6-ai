9592c14d2492ff4b7d650ba8403cde0f63c32467
bina <dhtpwjd26122@gmail.com>
2026-02-24 01:07:41 +0900

    feat: local DB -> collection 생성

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.services.embedding_service import get_embedding_service
-
 from app.routers import qwen_router, test_router
-
+from app.vectorDB.qdrant_setting import qdrant_setting
 load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    qdrant_setting()
     get_embedding_service()
     yield
 
--- app/vectorDB/qdrant_setting.py
+++ app/vectorDB/qdrant_setting.py
@@ -7,27 +7,28 @@ import os
 load_dotenv()
 
 mode = os.getenv("QDRANT_MODE")
-client = QdrantClient(host="localhost", port=6333)
+#client = QdrantClient(host="localhost", port=6333)
 
-if mode == "local":
+def qdrant_setting():
     client = QdrantClient(host="localhost", port=6333)
 
-# 중복 테스트
-COLLECTION_NAME = "billage_items"
-if not client.collection_exists(collection_name=COLLECTION_NAME):
-    client.create_collection(
-        collection_name=COLLECTION_NAME,
-        vectors_config={
-            "dino": models.VectorParams(
-                size=1024,
-                distance=models.Distance.COSINE
-            ),
-            "bingsu": models.VectorParams(
-                size=768,
-                distance=models.Distance.COSINE
-            ),
-        }
-    )
-    print(f"'{COLLECTION_NAME}' 컬렉션 생성")
-else:
-    print(f"'{COLLECTION_NAME}' 컬렉션 존재")
+    # 중복 테스트
+    collection_name = "billage_items"
+    if not client.collection_exists(collection_name=collection_name):
+        client.create_collection(
+            collection_name=collection_name,
+            vectors_config={
+                "dino_vec": models.VectorParams(
+                    size=1024,
+                    distance=models.Distance.COSINE
+                ),
+                "bingsu_vec": models.VectorParams(
+                    size=768,
+                    distance=models.Distance.COSINE
+                ),
+            }
+        )
+        print(f"'{collection_name}' 컬렉션 생성")
+    else:
+        print(f"'{collection_name}' 컬렉션 존재")
+
