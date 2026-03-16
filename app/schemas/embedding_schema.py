aadb3bd6690783cdef8774733061fd39c5a4b282
bina <dhtpwjd26122@gmail.com>
2026-03-17 01:52:04 +0900

    feat: needs 컬렉션 관련 스키마 정의

--- app/schemas/embedding_schema.py
+++ app/schemas/embedding_schema.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 from enum import Enum
+from typing import List
 
-
+# items 벡터DB
 class PriceUnit(str, Enum):
     HOUR = "HOUR"
     DAY = "DAY"
 
-# 벡터DB 저장
-# 요청
+# 저장 요청
 class ItemUpsertRequest(BaseModel):
     user_id: int
     group_id: int
@@ -16,3 +16,17 @@ class ItemUpsertRequest(BaseModel):
     price: int
     price_unit: PriceUnit
     file_key: str
+
+# needs 벡터DB
+class ActionUnit(str, Enum):
+    SEARCH = "SEARCH"
+    CLICK = "CLICK"
+
+class UserLogs(BaseModel):
+    type: ActionUnit
+    content: str
+
+class NeedsUpsertRequest(BaseModel):
+    user_id: int
+    #group_id: int
+    recent_logs: List[UserLogs]
